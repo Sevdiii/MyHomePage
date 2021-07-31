420a2b9b95bbac53a5b955abd9509dd525dcc278
Sevdi Azizi <you@example.com>
2021-07-31 15:00:45 +0200

    navbar and profil-info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfilInfoComponent } from './profil-info/profil-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfilInfoComponent
   ],
   imports: [
     BrowserModule,
