44ab002d21a32ab1741cf6f222cb11bc47582d13
Sevdi Azizi <you@example.com>
2021-08-10 13:16:33 +0200

    links update

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  JourneyPics = ['../assets/img/walk-line.png', '../assets/img/heart-line.png', '../assets/img/search-line.png', '../assets/img/flight-takeoff-line.png']
+  JourneyPics = ['../assets/img/walk-line.png', '..assets/img/heart-line.png', '..assets/img/search-line.png', '..assets/img/flight-takeoff-line.png']
 
   constructor() { }
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImpressumComponent } from './impressum/impressum.component';
-import { MySkillsComponent } from './my-skills/my-skills.component';
-import { MyWorkComponent } from './my-work/my-work.component';
 import { ProfilInfoComponent } from './profil-info/profil-info.component';
 
 
@@ -12,8 +10,6 @@ import { ProfilInfoComponent } from './profil-info/profil-info.component';
 
 const routes: Routes = [
   {path: '', component: ProfilInfoComponent},
-  {path: '', component: MySkillsComponent},
-  {path: '', component: MyWorkComponent},
   {path: 'impressum', component: ImpressumComponent}
 ];
 
