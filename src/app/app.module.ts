1247d6269a00a376e1aad988dad00601334e94bc
Sevdi Azizi <you@example.com>
2021-08-11 10:10:01 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { FooterComponent } from './footer/footer.component';
 import { ImpressumComponent } from './impressum/impressum.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 
@@ -25,12 +27,14 @@ import { ImpressumComponent } from './impressum/impressum.component';
     ContactMeComponent,
     FooterComponent,
     ImpressumComponent,
+    MatGridListModule
   
   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
